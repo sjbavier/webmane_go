3f31225e02390716589ef5d603adc0d75b1a5da1
sjbavier <sjbavier@gmail.com>
2024-04-14 21:04:10 -0400

    wip

---
+++ music/seed_music.go
@@ -0,0 +1,34 @@
+package music
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func SeedMusic(w http.ResponseWriter, r *http.Request) {
+
+	switch {
+	case r.Method == "POST":
+		CreateAllMusic(w, r)
+		return
+	case r.Method == "PUT":
+		RefreshAllMusic(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(fmt.Sprintf("Method not supported %v", r.Method)))
+		return
+	}
+}
+
+func CreateAllMusic(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Posted to seed"))
+}
+
+func RefreshAllMusic(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Posted to seed"))
+}
--- server.go
+++ server.go
@@ -35,6 +35,7 @@ func main() {
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 	http.HandleFunc("/music", music.GetMusic)
+	http.HandleFunc("/music/seed", music.SeedMusic)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
