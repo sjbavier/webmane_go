25dd7e609382ae8f3891d2bebdf41fb9d8a916e5
sjbavier <sjbavier@gmail.com>
2024-05-14 21:14:19 -0400

    cleanup

--- server.go
+++ server.go
@@ -36,15 +36,12 @@ func main() {
 		log.Fatalf("command failed to execute  %v", cmd_err)
 	}
 
-	// cmd.Execute()
-
-	// srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: dbPool}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 	http.HandleFunc("/music", music.GetMusic)
-	http.HandleFunc("/music/seed", music.SeedMusic(dbPool))
+	// http.HandleFunc("/music/seed", music.SeedMusic(dbPool))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
