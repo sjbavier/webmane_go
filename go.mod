6de98da2d8905ba27cc5519e420ca2db127c379d
sjbavier <sjbavier@gmail.com>
2024-05-18 21:58:05 -0400

    cors

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.21.6
 
 require (
 	github.com/99designs/gqlgen v0.17.44
+	github.com/go-chi/chi v1.5.5
+	github.com/gorilla/websocket v1.5.0
 	github.com/jackc/pgx/v4 v4.18.3
+	github.com/rs/cors v1.11.0
 	github.com/spf13/cobra v1.8.0
+	github.com/u2takey/ffmpeg-go v0.5.0
 	github.com/vektah/gqlparser/v2 v2.5.11
 )
 
@@ -14,7 +18,6 @@ require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
@@ -30,10 +33,8 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/urfave/cli/v2 v2.27.1 // indirect
-	github.com/vansante/go-ffprobe v1.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.20.0 // indirect
 	golang.org/x/mod v0.14.0 // indirect
--- server.go
+++ server.go
@@ -9,7 +9,12 @@ import (
 	"webmane_go/graph"
 	"webmane_go/music"
 
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
@@ -36,13 +41,34 @@ func main() {
 		log.Fatalf("command failed to execute  %v", cmd_err)
 	}
 
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://127.0.0.1:5173"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{DB: dbPool}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-	http.HandleFunc("/music", music.GetMusic(dbPool))
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "webmane.net"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+	router.HandleFunc("/music", music.GetMusic(dbPool))
 	// http.HandleFunc("/music/seed", music.SeedMusic(dbPool))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
